a4ac1f16b290aa2080a4c142a56149fc46c65f20
flemmerz <flemming@riskrails.com>
2024-12-09 19:42:04 +0000

    Create Dashboard.js

---
+++ src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>User Dashboard</h2>
+      <p>View your contributions and rewards here.</p>
+    </div>
+  );
+}
+
+export default Dashboard;
