6c6bcc3ee1e72d4dd31f914363a0e090b9933217
flemmerz <flemming@riskrails.com>
2024-12-09 19:41:17 +0000

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Dashboard from "./components/Dashboard";
+import DataSubmit from "./components/DataSubmit";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Mutual Insurance Platform</h1>
+      <Dashboard />
+      <DataSubmit />
+    </div>
+  );
+}
+
+export default App;
